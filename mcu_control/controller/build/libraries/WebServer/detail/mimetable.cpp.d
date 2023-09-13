76eee527da047d4cf556c9b513174b0d2ef3f797
Alicino <victor.alicino@gmail.com>
2023-09-13 17:26:36 -0300

    .

--- mcu_control/controller/build/libraries/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-C:\Users\victo\Documents\GitRepos\BLDCMiniVehicle\mcu_control\controller\build\libraries\WebServer\detail\mimetable.cpp.o: \
- C:\Users\victo\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\libraries\WebServer\src\detail\mimetable.cpp \
- C:\Users\victo\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\libraries\WebServer\src\detail\mimetable.h \
- C:\Users\victo\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.11\cores\esp32/pgmspace.h
--- mcu_control/controller/build/sketch/wireless_conn.hpp
+++
@@ -1,8 +0,0 @@
-#line 1 "C:\\Users\\victo\\Documents\\GitRepos\\BLDCMiniVehicle\\mcu_control\\controller\\wireless_conn.hpp"
-#pragma once
-
-#include <WiFi.h> 
-
-void wifi_start();
-
-void web_server_start();
